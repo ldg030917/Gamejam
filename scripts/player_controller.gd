fcd09e6c450fa04cde5b1f69bd27cf146d4e3899
^_^ <park060115@naver.com>
2025-05-24 16:03:56 +0900

    Player component complete

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,52 +1,74 @@
 extends CharacterBody2D
 
-
-
-func _ready() -> void:
-	pass
+@export var speed = 200
+@export var n_particle = 120
+var emit_on = false
+var R = false
+var G = false
+var B = false
+@onready var wave = preload("res://scenes/wave.tscn")
+@onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	velocity = Input.get_vector("Left", "Right", "Up", "Down") * speed
+	if emit_on :
+		change_arrow()
+	else :
+		velocity = Input.get_vector("Left", "Right", "Up", "Down").normalized()
 	
 	if velocity:
-		if velocity.x > 0:
+		if velocity.x > 0 :
 			$AnimatedSprite2D.play("right")
-		elif velocity.x < 0:
+		elif velocity.x < 0 :
 			$AnimatedSprite2D.play("left")
-		elif velocity.y > 0:
+		elif velocity.y > 0 :
 			$AnimatedSprite2D.play("down")
-		elif velocity.y < 0:
+		elif velocity.y < 0 :
 			$AnimatedSprite2D.play("up")
 	else:
 		$AnimatedSprite2D.play("idle")
 			
-	handle_collision(move_and_collide(velocity * delta))
+	handle_collision(move_and_collide(velocity * speed * delta))
 
 func handle_collision(res) -> void:
 	if res:
 		pass
-@export var speed = 100
-@export var n_particle = 120
-var emit_on = false
-@onready var wave = preload("res://scenes/wave.tscn")
-	
-func _process(delta):
-	if Input.is_action_pressed("Left"):
-		position.x -= speed * delta
-	if Input.is_action_pressed("Right"):
-		position.x += speed * delta
-	if Input.is_action_pressed("Up"):
-		position.y -= speed * delta
-	if Input.is_action_pressed("Down"):
-		position.y += speed * delta
-		
-	if emit_on and $WaveCool.time_left == 0:	
-		var wave = wave.instantiate() as Node2D
-		get_tree().root.add_child(wave)
-		wave.launch(global_position)
-		$WaveCool.start()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_pressed("Emit") :
 		emit_on = !emit_on
+	if Input.is_action_pressed("Blue") :
+		B = !B
+	if Input.is_action_pressed("Green") :
+		G = !G
+	if Input.is_action_pressed("Red") :
+		R = !R
+	sprite.modulate.b = 1.0 if B else 0.5
+	sprite.modulate.g = 1.0 if G else 0.5
+	sprite.modulate.r = 1.0 if R else 0.5
+	
+
+func _process(delta: float) -> void:
+	if emit_on and $WaveCool.time_left == 0 :	
+		var wave = wave.instantiate() as Node2D
+		get_tree().root.add_child(wave)
+		wave.launch(global_position, B, G, R)
+		$WaveCool.start()
 		
+func change_arrow() :
+	if B and G and R :
+		velocity = Vector2(0, 0)
+	elif B and G:
+		velocity = Input.get_vector("Up", "Right", "Down", "Left").normalized()
+	elif G and R :
+		velocity = Input.get_vector("Up", "Down", "Left", "Right").normalized()
+	elif B and R :
+		velocity = Input.get_vector("Left", "Down", "Right", "Up").normalized()
+	elif B :
+		velocity = Input.get_vector("Left", "Right", "Down", "Up").normalized()
+	elif G :
+		velocity = Input.get_vector("Up", "Right", "Left", "Down").normalized()
+	elif R :
+		velocity = Input.get_vector("Left", "Down", "Up", "Right").normalized()
+	else :
+		velocity = Input.get_vector("Right", "Left", "Up", "Down").normalized()
+	
--- scripts/wave.gd
+++ scripts/wave.gd
@@ -1,10 +1,25 @@
 extends Node2D
 
+@onready var sprite = $Sprite2D
+
 func _process(delta: float) -> void:
-	scale += Vector2(2, 2)*delta
+	scale += Vector2(4, 4)*delta
+	sprite.modulate.a -= delta
 	
-func launch(origin : Vector2):
+func launch(origin : Vector2, B : bool, G : bool, R : bool):
 	global_position = origin
+	if B : 
+		sprite.modulate.b = 1
+	else :
+		sprite.modulate.b = 0.3
+	if G :
+		sprite.modulate.g = 1
+	else :
+		sprite.modulate.g = 0.3
+	if R :
+		sprite.modulate.r = 1
+	else :
+		sprite.modulate.r = 0.3
 
 func _on_vanish_timer_timeout() -> void:
 	queue_free()
