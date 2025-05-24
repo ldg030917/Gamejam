f0666d2ccde791e6d26c98a1d3e036521cad38d6
ldg030917 <127109929+ldg030917@users.noreply.github.com>
2025-05-24 11:38:44 +0900

    move

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -5,12 +5,23 @@ extends CharacterBody2D
 func _ready() -> void:
 	pass
 
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("Left"):
-		position.x -= speed * delta
-	if Input.is_action_pressed("Right"):
-		position.x += speed * delta
-	if Input.is_action_pressed("Up"):
-		position.y -= speed * delta
-	if Input.is_action_pressed("Down"):
-		position.y += speed * delta
+func _physics_process(delta: float) -> void:
+	velocity = Input.get_vector("Left", "Right", "Up", "Down") * speed
+	
+	if velocity:
+		if velocity.x > 0:
+			$AnimatedSprite2D.play("right")
+		elif velocity.x < 0:
+			$AnimatedSprite2D.play("left")
+		elif velocity.y > 0:
+			$AnimatedSprite2D.play("down")
+		elif velocity.y < 0:
+			$AnimatedSprite2D.play("up")
+	else:
+		$AnimatedSprite2D.play("idle")
+			
+	handle_collision(move_and_collide(velocity * delta))
+
+func handle_collision(res) -> void:
+	if res:
+		pass
